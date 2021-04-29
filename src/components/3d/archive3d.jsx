4f91b5e7af2777c17129f97945ef00dfda0cefe3
Alban-Brice Pimpaud <alban.pimpaud@archeo3d.net>
2021-04-29 18:57:47 +0200

    up

---
+++ src/components/3d/archive3d.jsx
@@ -0,0 +1,90 @@
+import React, { useRef, useEffect, useState } from "react";
+import { Vector3, Euler, MathUtils } from "three";
+import { useTexture } from "@react-three/drei";
+import { useStore } from "../../store";
+import archiveData  from "../../assets/json/archives.json";
+
+
+const Archive3D = (props) => {
+
+  const {name} = props
+
+  const archive = archiveData.find((arch) => arch.name === name);  
+
+
+    function CreateNothing(){
+        return (
+            <group></group>
+        )
+    }
+
+    function SetArchive(props){
+        const {archive} = props
+        const { X, Y, Z, rotX, rotY, rotZ, size, scale, width, height } = archive;
+
+        let taille = size * scale;
+        let ratio = height / width;
+      
+        const ref = useRef();      
+        const pos = new Vector3(X, Z, -Y);
+      
+        const rot = new Euler(
+          -MathUtils.degToRad(rotX) + Math.PI / 2,
+          MathUtils.degToRad(rotZ),
+          MathUtils.degToRad(rotY),
+          "XYZ"
+        );
+      
+        // OK approx
+        const rot3 = new Euler(
+          Math.PI / 2 - MathUtils.degToRad(rotX),
+          MathUtils.degToRad(rotZ),
+          MathUtils.degToRad(-rotY),
+          "XYZ"
+        );
+      
+        // OK pour PLAN
+        const rot2 = new Euler(
+          //Math.PI / 2 - MathUtils.degToRad(rotX),
+          //MathUtils.degToRad(rotZ),
+          //MathUtils.degToRad(-rotY),
+          -Math.PI / 2 + MathUtils.degToRad(rotX),
+          MathUtils.degToRad(rotY),
+          MathUtils.degToRad(rotZ),
+          "XYZ"
+        );
+      
+        // const quat = new Quaternion(0.238468, -0.665682, -0.680944, 0.190564);
+        const archImg = useTexture(`/img/${name}.jpg`);
+        return (
+            <mesh
+              ref={ref}
+              name={name}
+              rotation={[rot.x, rot.y, rot.z]}
+              position={[pos.x, pos.y, pos.z]}
+            >
+              <planeBufferGeometry attach="geometry" args={[taille, taille * ratio]} />
+              <meshStandardMaterial
+                attach="material"
+                color="white"
+                transparent={false}
+                opacity={1}
+                map={archImg}
+              />
+            </mesh>
+          );
+
+    }
+
+    return (
+        <SetArchive archive={archive} />
+    )
+  
+
+
+ 
+
+
+};
+
+export { Archive3D };
--- src/systems/scene3d.jsx
+++ src/systems/scene3d.jsx
@@ -3,9 +3,10 @@ import { Canvas } from "@react-three/fiber";
 import { OrbitControls } from "@react-three/drei";
 
 import { Orco10K } from "../components/3d/orco";
+import {Archive3D} from "../components/3d/archive3d"
 import { Lights } from "./lights";
 
-const archives = ["Orco-01-0001", "Orco-01-0002", "Orco-04-0015"];
+const archives = ["Orco-01-0002", "Orco-04-0015"];
 
 export default function Scene3D() {
   return (
@@ -21,8 +22,21 @@ export default function Scene3D() {
 
       <Suspense name="suspense" fallback={null}>
         <Orco10K />
+        {archives.map((item, index) => (
+          <Archive3D key={index} name={item}/>
+        ))}
+        
       </Suspense>
-      <OrbitControls />
+      <OrbitControls 
+          rotateSpeed={0.4}
+          enableDamping={true}
+          dampingFactor={0.4}
+          enableZoom={true}
+          enablePan={true}
+          enableKeys={true}
+          screenSpacePanning={true}
+          maxDistance={200}
+      />
     </Canvas>
   );
 }
